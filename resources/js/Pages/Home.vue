ae571e97b5277e38f3c3e2426a7017f11c10ef40
aplactuan <aplactuan@gmail.com>
2023-06-18 23:30:47 +0800

    Testing a single page in Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps({
+        name: String,
+        frameworks: Array
+    })
+</script>
+<template>
+    <h1>Hello {{ name}}</h1>
+    <p>This is a website that uses the following</p>
+    <ul>
+        <li v-for="framework in frameworks">{{ framework }}</li>
+    </ul>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,15 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+    return Inertia::render('Home', [
+        'name' => 'Adrian Lactuan',
+        'frameworks' => ['Laravel', 'Inertia', 'Vue']
     ]);
 });
 
