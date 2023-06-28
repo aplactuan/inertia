62569330304a69b486e35239c92a80b851104d35
aplactuan <aplactuan@gmail.com>
2023-06-28 21:44:41 +0800

    How to style active links

--- resources/js/Pages/Shared/Nav.vue
+++ resources/js/Pages/Shared/Nav.vue
@@ -3,10 +3,13 @@ import {Link} from "@inertiajs/vue3";
 </script>
 
 <template>
-    <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/users">Users</Link></li>
-        <li><Link href="/settings">Settings</Link></li>
-        <li><Link href="/logout-test" method="post" as="button">Logout</Link></li>
-    </ul>
+    <div class="grid align-center">
+        <ul class="list-disc">
+            <li :class="{'font-bold': $page.component == 'Home'}"><Link href="/">Home</Link></li>
+            <li :class="{'font-bold': $page.component == 'Users'}"><Link href="/users">Users</Link></li>
+            <li :class="{'font-bold': $page.component == 'Settings'}"><Link href="/settings">Settings</Link></li>
+            <li><Link href="/logout-test" method="post" as="button" class="list-disc">Logout</Link></li>
+        </ul>
+    </div>
+
 </template>
