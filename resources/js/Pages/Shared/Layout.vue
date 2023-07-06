6dc5e878237d0d6d0b7fdb25fdb292be91ec212f
aplactuan <aplactuan@gmail.com>
2023-07-06 19:15:54 +0800

    Add a little bit of design on header

--- resources/js/Pages/Shared/Layout.vue
+++ resources/js/Pages/Shared/Layout.vue
@@ -2,6 +2,14 @@
     import Nav from "./Nav.vue"
 </script>
 <template>
-    <Nav />
+    <header>
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between">
+                <h1>App Name</h1>
+                <Nav />
+            </div>
+        </div>
+    </header>
+
     <slot />
 </template>
--- resources/js/Pages/Shared/Nav.vue
+++ resources/js/Pages/Shared/Nav.vue
@@ -3,12 +3,12 @@ import {Link} from "@inertiajs/vue3";
 </script>
 
 <template>
-    <div class="grid align-center">
-        <ul class="list-disc">
-            <li :class="{'font-bold': $page.component == 'Home'}"><Link href="/">Home</Link></li>
-            <li :class="{'font-bold': $page.component == 'Users'}"><Link href="/users">Users</Link></li>
-            <li :class="{'font-bold': $page.component == 'Settings'}"><Link href="/settings">Settings</Link></li>
-            <li><Link href="/logout-test" method="post" as="button" class="list-disc">Logout</Link></li>
+    <div>
+        <ul class="flex space-x-4">
+            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Home'}"><Link href="/">Home</Link></li>
+            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Users'}"><Link href="/users">Users</Link></li>
+            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Settings'}"><Link href="/settings">Settings</Link></li>
+            <li class="text-blue-800"><Link href="/logout-test" method="post" as="button" class="list-disc">Logout</Link></li>
         </ul>
     </div>
 
