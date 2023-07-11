b8db61e67a4557137d548ff0210655b1c21a5a3c
aplactuan <aplactuan@gmail.com>
2023-07-11 18:27:44 +0800

    Add a component for menu item

---
+++ resources/js/Pages/Shared/Menu/MenuItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import {Link} from "@inertiajs/vue3";
+
+    import {defineProps} from "vue";
+
+    const props = defineProps({
+        link: String,
+        cmpnt: String
+    })
+</script>
+
+<template>
+    <li class="text-blue-800" :class="{'font-bold': $page.component == cmpnt}"><Link :href=link><slot /></Link></li>
+</template>
--- resources/js/Pages/Shared/Nav.vue
+++ resources/js/Pages/Shared/Nav.vue
@@ -1,13 +1,14 @@
 <script setup>
 import {Link} from "@inertiajs/vue3";
+import MenuItem from "./Menu/MenuItem.vue"
 </script>
 
 <template>
     <div>
         <ul class="flex space-x-4">
-            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Home'}"><Link href="/">Home</Link></li>
-            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Users'}"><Link href="/users">Users</Link></li>
-            <li class="text-blue-800" :class="{'font-bold': $page.component == 'Settings'}"><Link href="/settings">Settings</Link></li>
+            <MenuItem link="/" cmpnt="Home">Home</MenuItem>
+            <MenuItem link="/users" cmpnt="Users">Users</MenuItem>
+            <MenuItem link="/settings" cmpnt="Settings">Settings</MenuItem>
             <li class="text-blue-800"><Link href="/logout-test" method="post" as="button" class="list-disc">Logout</Link></li>
         </ul>
     </div>
